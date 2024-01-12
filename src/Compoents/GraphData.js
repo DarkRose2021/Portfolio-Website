51d052139682166faf9bb7618f43e1af676c7a76
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-01-12 16:29:07 -0700

    moved the data array to it's own file

---
+++ src/Compoents/GraphData.js
@@ -0,0 +1,42 @@
+export const data = [
+	{
+		name: "React.js",
+		level: 6,
+		amt: 6,
+	},
+	{
+		name: "SCSS",
+		level: 7,
+		amt: 7,
+	},
+	{
+		name: "JavaScript",
+		level: 8,
+		amt: 8,
+	},
+	{
+		name: "Angular",
+		level: 4,
+		amt: 4,
+	},
+	{
+		name: "Java",
+		level: 3,
+		amt: 3,
+	},
+	{
+		name: "Python",
+		level: 4,
+		amt: 4,
+	},
+	{
+		name: "HTML",
+		level: 10,
+		amt: 10,
+	},
+	{
+		name: "CSS",
+		level: 10,
+		amt: 10,
+	},
+];
--- src/Compoents/Skills.js
+++ src/Compoents/Skills.js
@@ -1,63 +1,14 @@
-import React, { PureComponent } from "react";
-import {
-	BarChart,
-	Bar,
-	XAxis,
-	YAxis,
-	CartesianGrid,
-	Tooltip,
-} from "recharts";
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 
-const data = [
-	{
-		name: "React.js",
-		level: 6,
-		amt: 6,
-	},
-	{
-		name: "SCSS",
-		level: 7,
-		amt: 7,
-	},
-	{
-		name: "JavaScript",
-		level: 8,
-		amt: 8,
-	},
-	{
-		name: "Angular",
-		level: 4,
-		amt: 4,
-	},
-	{
-		name: "Java",
-		level: 3,
-		amt: 3,
-	},
-	{
-		name: "Python",
-		level: 4,
-		amt: 4,
-	},
-	{
-		name: "HTML",
-		level: 10,
-		amt: 10,
-	},
-	{
-		name: "CSS",
-		level: 10,
-		amt: 10,
-	},
-];
+import { data } from "./GraphData";
 
-export default class Skills extends PureComponent {
-	render() {
-		return (
-			<div className="centerChart">
-				<div className="chart-container">
-					<h1>Skills</h1>
-					<div>
+const Skills = () => {
+	return (
+		<div className="centerChart">
+			<div className="chart-container">
+				<h1>Skills</h1>
+				<div>
 					<BarChart
 						width={1000}
 						height={500}
@@ -76,8 +27,9 @@ export default class Skills extends PureComponent {
 						<Bar dataKey="level" fill="#B99AC4" />
 					</BarChart>
 				</div>
-				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+export default Skills;
