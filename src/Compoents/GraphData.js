029bdeaca2265366d93a26f911b0b98182188447
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2024-01-20 19:23:23 -0700

    added a bar chart and mobile things

--- src/Compoents/GraphData.js
+++ src/Compoents/GraphData.js
@@ -1,4 +1,4 @@
-export const data = [
+export const pieData = [
 	{ x: 1, y: 2, label: "Java" },
 	{ x: 2, y: 3, label: "Python" },
 	{ x: 4, y: 5, label: "Angular" },
@@ -6,4 +6,14 @@ export const data = [
 	{ x: 8, y: 9, label: "JavaScript" },
 	{ x: 9, y: 10, label: "HTML" },
 	{ x: 10, y: 11, label: "CSS/SCSS" },
+];
+
+export const data = [
+	{ x: 1, y: 2, fill: "#D7494B" },
+	{ x: 2, y: 3, fill: "#F3722C" },
+	{ x: 3, y: 4, fill: "#F9C74F" },
+	{ x: 4, y: 5, fill: "#90BE6D" },
+	{ x: 5, y: 8, fill: "#43AA8B" },
+	{ x: 6, y: 10, fill: "#577590" },
+	{ x: 7, y: 10, fill: "#6D549F" },
 ];
--- src/Compoents/Skills.js
+++ src/Compoents/Skills.js
@@ -1,24 +1,94 @@
-import React from "react";
-import { VictoryPie } from "victory";
-import { data } from "./GraphData";
+import React, { useState, useEffect } from "react";
+import {
+	VictoryPie,
+	VictoryBar,
+	VictoryTheme,
+	VictoryChart,
+	VictoryAxis,
+} from "victory";
+import { data, pieData } from "./GraphData";
 
 const Skills = () => {
+	const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<div className="centerChart">
 			<div className="chart-container">
 				<h1>Skills</h1>
+				<h4>Measured with how comfortable I am using them</h4>
 				<div>
-					<VictoryPie
-						colorScale={["#D7494B", "#F3722C", "#F9C74F", "#90BE6D", "#43AA8B", "#577590", "#6D549F"]}
-						data={data}
-						width={500}
-						style={{
-							labels: {
-								fontSize: 20,
-								fill: "#e5e5e5",
-							},
-						}}
-					/>
+					{isMobile ? (
+						<VictoryPie
+							colorScale={[
+								"#D7494B",
+								"#F3722C",
+								"#F9C74F",
+								"#90BE6D",
+								"#43AA8B",
+								"#577590",
+								"#6D549F",
+							]}
+							data={pieData}
+							width={500}
+							style={{
+								labels: {
+									fill: "#e5e5e5",
+								},
+							}}
+						/>
+					) : (
+						<VictoryChart
+							width={1000}
+							theme={VictoryTheme.material}
+							domainPadding={{ x: 35 }}
+						>
+							<VictoryAxis
+								style={{
+									tickLabels: {
+										fontSize: 20,
+										fill: "#e5e5e5",
+										padding: 5,
+									},
+								}}
+								tickValues={[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]}
+								tickFormat={[
+									"Java",
+									"Python",
+									"Angular",
+									"React.js",
+									"JavaScript",
+									"HTML",
+									"CSS/SCSS",
+								]}
+							/>
+
+							<VictoryBar
+								barWidth={70}
+								style={{
+									data: {
+										fill: ({ datum }) => datum.fill,
+									},
+									labels: {
+										fontSize: 16,
+										fill: "#e5e5e5",
+									},
+								}}
+								data={data}
+							/>
+						</VictoryChart>
+					)}
 				</div>
 			</div>
 		</div>
