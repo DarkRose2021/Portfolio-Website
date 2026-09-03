7a94db6c016cc62567a480c960593605b6be48d9
Katie King <89149647+DarkRose2021@users.noreply.github.com>
2026-09-03 16:50:37 -0500

    added data to the navigation
    Once again I'll design it later

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
+import { NavLink } from "react-router-dom";
+export default function Navigation() {
+	return (
+		<header className="nav">
+			<div className="container nav-inner">
+				<NavLink to="/" className="logo">
+					<span>Kathrine King</span>
+				</NavLink>
+				<nav>
+					<NavLink to="/" end>
+						Home
+					</NavLink>
+					<NavLink to="/work">Work</NavLink>
+					<NavLink to="/about">About</NavLink>
+					<NavLink to="/resume">Resume</NavLink>
+					<NavLink to="/contact" className="nav-cta">
+						Contact
+					</NavLink>
+				</nav>
+			</div>
+		</header>
+	);
 }
-
-export default Navigation
