ef6134ff3b2d5f7c6d770900e19c47f3f6d6750a
Katie King <89149647+DarkRose2021@users.noreply.github.com>
2026-09-03 16:48:30 -0500

    Added the footer text
    I'll design it later

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+export default function Footer() {
+	return (
+		<footer>
+			<div className="container footer-inner">
+				<div>
+					<strong>Kathrine King</strong>
+					<p>Website Designer & UI/UX Designer</p>
+				</div>
+				<div className="footer-links">
+					<a href="mailto:katiek2128@gmail.com">Email</a>
+					<a
+						href="https://github.com/DarkRose2021"
+						target="_blank"
+						rel="noreferrer"
+					>
+						GitHub
+					</a>
+					<a href="/contact">Contact</a>
+				</div>
+				<small>&copy; {new Date().getFullYear()} Kathrine King</small>
+			</div>
+		</footer>
+	);
 }
-
-export default Footer
